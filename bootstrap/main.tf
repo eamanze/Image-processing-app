73d89c33711e6f50b6a17159554c04feb3fe4dbf
Cloudhight Admin <cloudhightadmin@192.168.1.14>
2026-07-25 19:04:37 +0100

    Allow Terraform to inspect role profiles

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -215,6 +215,7 @@ data "aws_iam_policy_document" "deployment" {
       "iam:GetRole",
       "iam:GetRolePolicy",
       "iam:ListAttachedRolePolicies",
+      "iam:ListInstanceProfilesForRole",
       "iam:ListRolePolicies",
       "iam:PassRole",
       "iam:PutRolePolicy",
